ec3150df5a2232eb9931eead8a0db4bc2d4f23b2
sk185488 <sk185488@ncr.com>
2020-02-24 22:01:56 +0530

    file operations

--- Chapters-18-to-22/word-count-in-file.c
+++ Chapters-18-to-22/word-count-in-file.c
@@ -4,6 +4,37 @@
 int main()
 {
   FILE *fp;
+  int wordCount=0;
+  char previousChar;
+
+  fp = fopen("names.txt", "r");
+  if(fp == NULL)
+  {
+    printf("can't open file");
+    exit(1);
+  }
+  else
+  {
+    
+    while(1)
+    {
+      char ch;
+      ch = fgetc(fp);
+
+      if(previousChar == ch == ' ')
+        printf("multiple spaces are present between words\n");
+      else{
+        previousChar = ch;
+        if(ch == ' ')
+          wordCount++;
+        else if(ch == EOF)
+          break;
+      }
+    }
+  
+  }
+
+  printf("Number of words in the file are %d", wordCount);
 
   return 0;
 }
